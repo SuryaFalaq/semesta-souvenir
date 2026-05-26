370df271797eb26329ed7e6700c27f25a1eb6465
Semesta Souvenir <semestasouvenir@gmail.com>
2026-05-26 18:44:53 +0700

    Remove unused getApiUrl import

--- src/pages/admin/AdminProducts.jsx
+++ src/pages/admin/AdminProducts.jsx
@@ -6,7 +6,7 @@ import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
 import { Textarea } from '../../components/ui/textarea';
 import { Label } from '../../components/ui/label';
-import { getImageUrl, getApiUrl } from '../../config';
+import { getImageUrl } from '../../config';
 
 const AdminProducts = () => {
   const { language } = useLanguage();
